2442f75916783dd99b0aa2902429b6ebdb65c283
zzx <zzx@YYGZ-54>
2017-11-27 18:34:17 +0800

    配置添加

--- src/main/java/com/appleframework/boot/demo/web/DemoController.java
+++ src/main/java/com/appleframework/boot/demo/web/DemoController.java
@@ -1,16 +1,17 @@
 package com.appleframework.boot.demo.web;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.biz.banana.bean.User;
+import org.biz.banana.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.appleframework.boot.demo.model.Demo;
 import com.appleframework.web.springmvc.controller.BaseController;
 
 
@@ -19,14 +20,14 @@ import com.appleframework.web.springmvc.controller.BaseController;
 public class DemoController extends BaseController {
 
 	private String viewModel = "demo/";
+	
+	@Resource
+	private UserService userService;
 
 	@RequestMapping(value = "/list")
 	public String list(Model model, HttpServletRequest request,
 			HttpServletResponse response) {		
-		List<Demo> list = new ArrayList<Demo>();
-		for (int i = 1; i <= 100; i++) {
-			list.add(Demo.create(i, "demo-" + i, i));
-		}
+		List<User> list = userService.findList();
 		model.addAttribute("list", list);
 		return viewModel + "list";
 	}
