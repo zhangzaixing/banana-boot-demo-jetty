1192aff5356ca50f424a114437d161536c53c42b
zzx <zzx@YYGZ-54>
2017-12-26 09:38:04 +0800

    发送kafka

--- src/main/java/com/appleframework/boot/demo/web/DemoController.java
+++ src/main/java/com/appleframework/boot/demo/web/DemoController.java
@@ -7,18 +7,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.biz.banana.bean.User;
+import org.biz.banana.provider.util.Constant;
 import org.biz.banana.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+
+import com.appleframework.jms.kafka.producer.KafkaMessageProducer2;
+import com.appleframework.model.DefaultDto;
 import com.appleframework.web.springmvc.controller.BaseController;
 
 
 @Controller
 @RequestMapping("/demo")
 public class DemoController extends BaseController {
-
+	
+	@Resource
+	private KafkaMessageProducer2 kafkaMessageProducer2;
+	
 	private String viewModel = "demo/";
 	
 	@Resource
@@ -30,5 +38,27 @@ public class DemoController extends BaseController {
 		List<User> list = userService.findList();
 		model.addAttribute("list", list);
 		return viewModel + "list";
+	}
+	
+	@RequestMapping(value = "/kafka")
+	@ResponseBody
+	public void kafka(Model model, HttpServletRequest request,
+			HttpServletResponse response) {		
+		for (int i = 1; i <= 10000; i++) {
+			kafkaMessageProducer2.sendObject(Constant.TOPIC_DEMO2, new User(i, "zzx-demo-" + i, i));
+		}
+	}
+	
+	@RequestMapping(value = "/kafkaDto")
+	@ResponseBody
+	public void kafkaDto(Model model, HttpServletRequest request,
+			HttpServletResponse response) {		
+		for (int i = 1; i <= 10000; i++) {
+			DefaultDto dto = DefaultDto.create();
+			dto.put("userId", i);
+			dto.put("name", "zzx-demo-" + i);
+			dto.put("age", i);
+			kafkaMessageProducer2.sendObject(Constant.TOPIC_DEMO3, dto);
+		}
 	}
 }
